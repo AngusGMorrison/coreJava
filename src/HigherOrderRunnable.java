acb372b9401e03b3b13abeb81991db98d1385d55
Angus Morrison <contact@angus-morrison.com>
2020-01-06 11:06:07 +0000

    ch03 ex14

---
+++ src/HigherOrderRunnable.java
@@ -0,0 +1,20 @@
+
+public class HigherOrderRunnable {
+
+	public static void main(String[] args) {
+		Runnable task1 = () -> System.out.println("task 1");
+		Runnable task2 = () -> System.out.println("task 2");
+		Runnable[] runnables = { task1, task2 };
+		Runnable merged = mergeRunnables(runnables);
+		merged.run();
+	}
+	
+	public static Runnable mergeRunnables(Runnable[] runnables) {
+		return () -> {
+			for (Runnable task : runnables) {
+				task.run();
+			}
+		};
+	}
+
+}
