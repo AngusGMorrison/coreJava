eed2f1024a2f5258e69402de6b997a4284eaf748
Angus Morrison <contact@angus-morrison.com>
2020-01-09 13:06:23 +0000

    Improvements to try-catch structure

--- src/ch05/ex01/DoubleReader.java
+++ src/ch05/ex01/DoubleReader.java
@@ -4,12 +4,11 @@ import java.util.ArrayList;
 import java.io.FileNotFoundException;
 import java.io.File;
 import java.util.Scanner;
-import java.util.InputMismatchException;
 
 public class DoubleReader {
 	public static void main(String[] args) {
 		try {
-			ArrayList<Double> doubles = readValues("src/ch05/ex01/resources/doubles.txt");
+			readValues("src/ch05/ex01/resources/doubles.txt");
 		} catch (FileNotFoundException | IllegalArgumentException e) {
 			System.out.println(e.getMessage());
 		}	
@@ -23,12 +22,13 @@ public class DoubleReader {
 			}
 			
 			ArrayList<Double> doubles = new ArrayList<>();
-			Scanner scanner = new Scanner(file);
-			while (scanner.hasNext()) {
-				if (!scanner.hasNextDouble()) {
-					throw new IllegalArgumentException("File contains non-floating point values");
+			try (Scanner scanner = new Scanner(file)) {
+				while (scanner.hasNext()) {
+					if (!scanner.hasNextDouble()) {
+						throw new IllegalArgumentException("File contains non-floating point values");
+					}
+					doubles.add(scanner.nextDouble());
 				}
-				doubles.add(scanner.nextDouble());
 			}
 			return doubles;
 	}
