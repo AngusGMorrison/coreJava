9eb97708184ff47fbb59a86a33f7f1201ca9088e
Angus Morrison <contact@angus-morrison.com>
2020-01-17 12:23:05 +0000

    Saved changes

--- src/ch06/ex06/ArrayListWildcards.java
+++ src/ch06/ex06/ArrayListWildcards.java
@@ -3,11 +3,11 @@ package ch06.ex06;
 import java.util.ArrayList;
 
 public class ArrayListWildcards {
-	public static <E> void appendExtends(ArrayList<E> receiver, ArrayList<? extends E> listToAppend) {
-		receiver.addAll(listToAppend);
+	public static <E> void appendExtends(ArrayList<E> destination, ArrayList<? extends E> source) {
+		destination.addAll(source);
 	}
 	
-	public static <E> void appendSuper(ArrayList<? super E> receiver, ArrayList<E> listToAppend) {
-		receiver.addAll(listToAppend);
+	public static <E> void appendSuper(ArrayList<? super E> destination, ArrayList<E> source) {
+		destination.addAll(source);
 	}
 }
