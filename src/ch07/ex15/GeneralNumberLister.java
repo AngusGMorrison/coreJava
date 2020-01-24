7ea7f2fe386d6a42b1c7dd4ada315ace31da333c
Angus Morrison <contact@angus-morrison.com>
2020-01-24 17:55:07 +0000

    ch07 ex16

--- src/ch07/ex15/GeneralNumberLister.java
+++ src/ch07/ex15/GeneralNumberLister.java
@@ -3,14 +3,22 @@ package ch07.ex15;
 import java.util.AbstractList;
 import java.util.List;
 import java.util.function.IntFunction;
+import ch07.ex13.Cache;
 
 public class GeneralNumberLister {
 	public static <R> List<R> createList(IntFunction<R> function) {
 		
 		return new AbstractList<>() {
+			private Cache<Integer, R> cache = new Cache(100);
+			
 			@Override
 			public R get(int index) {
-				return function.apply(index);
+				if (cache.containsKey(index)) {
+					return cache.get(index);
+				}
+				
+				cache.put(index, function.apply(index));
+				return cache.get(index);
 			}
 			
 			@Override
